2973bfae2377bb6f747f36eec346e2cd57f9aa71
Emad Gasser <omda.g@outlook.com>
2026-02-03 16:34:35 +0000

    fixed small bug in algorithm manager file imports

--- algorithm/algorithm_manager.py
+++ algorithm/algorithm_manager.py
@@ -1,29 +1,36 @@
 import streamlit as st
-from algorithm import python_dp, python_numpy, cpp
-from algorithm.volatility import crr_up_down
+from pricing import dp_price, np_price, cpp_price, black_scholes_price
+from .volatility import crr_up_down
+from algorithm.arbitrage import put_call_parity
 
 
 PRICERS = {
-    "Python DP": python_dp.dp_price,
-    "NumPy Vectorization": python_numpy.np_price,
-    "Python NumPy": python_numpy.np_price,  # alias
-    "C++": cpp.cpp_price,
+    "Python DP": dp_price,
+    "NumPy Vectorization": np_price,
+    "C++": cpp_price,
 }
 
 
-def alogorithm_manager(S0, K, T, r, N, vol, opttype, optclass, method=None):
+def alogorithm_manager(S0, K, T, r, N, optclass, method=None):
     method = method or st.session_state.get("method", "Python DP")
     if isinstance(method, tuple):
         method = method[0]
+    
+    vol = st.session_state.get("volatility", 0.2)  # Default volatility if not set
 
     dt = T / N
     u, d = crr_up_down(vol, dt)
     pricer = PRICERS.get(method)
 
     if pricer:
-        price = pricer(S0, K, T, r, N, u, d, opttype, optclass)
+        call = pricer(S0, K, T, r, N, u, d, opttype="C", optclass=optclass)
+        put = pricer(S0, K, T, r, N, u, d, opttype="P", optclass=optclass)
     else:
-        price = None
+        call = None
+        put = None
 
-    st.session_state["option_price"] = price
-    return price
+    pc_parity = put_call_parity(S0, K, T, r, call, put, optclass)
+
+    st.session_state["arb_metrics"] = {"pc_parity": pc_parity}
+    st.session_state["option_price"] = [call, put]
+    return call, put
